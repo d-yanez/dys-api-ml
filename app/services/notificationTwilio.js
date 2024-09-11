1e1fbeb9e1aed036748b32b1b3ae389db1f0c7d7
dyaneza <dyanez@ripley.com>
2024-09-10 23:59:42 -0300

    delete lib twilio

--- app/services/notificationTwilio.js
+++ app/services/notificationTwilio.js
@@ -2,28 +2,28 @@
 const {ACCOUNT_SID_TWILIO,AUTH_TOKEN_TWILIO,FROM_PHONE_NUMBER,TO_PHONE_NUMBER} = process.env
 
 
-const client = require('twilio')(ACCOUNT_SID_TWILIO, AUTH_TOKEN_TWILIO);
+//const client = require('twilio')(ACCOUNT_SID_TWILIO, AUTH_TOKEN_TWILIO);
 exports.notification = async (req, res) => {
 
     let cajas = '*C003, C027*'
     let qty = '*5*'
     let sku = '2125798784'
     //let message = `Producto en cajas ${cajas} sacar ${qty} unidades`
-    let message = `Sku https://railway-node-express-app.up.railway.app/views?sku=${sku} agotado en Mercado Libre Full`
+    /*let message = `Sku https://railway-node-express-app.up.railway.app/views?sku=${sku} agotado en Mercado Libre Full`
     client.messages
     .create({
        from: `whatsapp:${FROM_PHONE_NUMBER}`,
        body: message,
        to: `whatsapp:${TO_PHONE_NUMBER}`
      })
-    .then(message => console.log(message.sid))
+    .then(message => console.log(message.sid))*/
     
     res.send({staus:'ok'})
 }
 
 exports.outStockNotification = async (sku) =>{
   console.log('outStockNotification - init')
-  let message = `Sku https://railway-node-express-app.up.railway.app/views?sku=${sku} agotado en Mercado Libre Full`
+  /*let message = `Sku https://railway-node-express-app.up.railway.app/views?sku=${sku} agotado en Mercado Libre Full`
   
   client.messages
   .create({
@@ -31,6 +31,6 @@ exports.outStockNotification = async (sku) =>{
      body: message,
      to: `whatsapp:${TO_PHONE_NUMBER}`
    })
-  .then(message => console.log(message.sid))
+  .then(message => console.log(message.sid))*/
   console.log('outStockNotification - end')
 }
